cdc6840fd1617a543a4fd7945971cf79fad3da56
petcebch <peter.cebula@spirago.ch>
2023-10-06 23:37:19 +0200

    adjust start

--- start.sh
+++ start.sh
@@ -15,5 +15,6 @@ fi
 
 source /root/miniconda3/etc/profile.d/conda.sh  # Adjust this path based on where Miniconda is installed
 conda activate privategpt
+pip install click
 python /ingest.py
 sleep infinity
